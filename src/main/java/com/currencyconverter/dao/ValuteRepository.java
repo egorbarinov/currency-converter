b8db6bc2c5fcda5adf943d0ba9d1165c33dad44a
Dell-PC <egor.barinov@gmail.com>
2021-01-25 14:21:01 +0300

    Optimized the method List<ValuteDto> getAll(): removed sorting

--- src/main/java/com/currencyconverter/dao/ValuteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.currencyconverter.dao;
-
-import com.currencyconverter.model.Valute;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ValuteRepository extends JpaRepository<Valute, Long> {
-
-}
